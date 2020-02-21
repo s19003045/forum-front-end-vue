077951860362110fab7e88a26d6bb05dc6a3d592
Gary Wu <s19003045@gmail.com>
2020-02-22 00:50:58 +0800

    新增 Navbar component，並依據使用者身份顯示適當的訊息

--- src/views/SignIn.vue
+++ src/views/SignIn.vue
@@ -1,13 +1,8 @@
 <template>
-  <div class="container py-5">
-    <form
-      class="w-100"
-      @submit.prevent.stop="handleSubmit"
-    >
+  <div class="container py-5 mt-5">
+    <form class="w-100" @submit.prevent.stop="handleSubmit">
       <div class="text-center mb-4">
-        <h1 class="h3 mb-3 font-weight-normal">
-          Sign In
-        </h1>
+        <h1 class="h3 mb-3 font-weight-normal">Sign In</h1>
       </div>
 
       <div class="form-label-group mb-2">
@@ -21,7 +16,7 @@
           placeholder="email"
           required
           autofocus
-        >
+        />
       </div>
 
       <div class="form-label-group mb-3">
@@ -34,27 +29,18 @@
           class="form-control"
           placeholder="Password"
           required
-        >
+        />
       </div>
 
-      <button
-        class="btn btn-lg btn-primary btn-block mb-3"
-        type="submit"
-      >
-        Submit
-      </button>
+      <button class="btn btn-lg btn-primary btn-block mb-3" type="submit">Submit</button>
 
       <div class="text-center mb-3">
         <p>
-          <router-link to="/signup">
-            Sign Up
-          </router-link>
+          <router-link to="/signup">Sign Up</router-link>
         </p>
       </div>
 
-      <p class="mt-5 mb-3 text-muted text-center">
-        &copy; 2017-2018
-      </p>
+      <p class="mt-5 mb-3 text-muted text-center">&copy; 2017-2018</p>
     </form>
   </div>
 </template>
