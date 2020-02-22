bbb09a4289c5914d6c757d6e154af98721d9e4f0
Gary Wu <s19003045@gmail.com>
2020-02-23 00:59:45 +0800

    載入 uuid 套件，為 navTab 編列 id

--- src/components/NavTabs.vue
+++ src/components/NavTabs.vue
@@ -1,16 +1,44 @@
 <template>
   <ul class="nav nav-tabs mb-4">
-    <li class="nav-item">
-      <router-link to="/restaurants" class="nav-link">首頁</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/restaurants/feeds" class="nav-link">最新動態</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/restaurants/top" class="nav-link">TOP 10 人氣餐廳</router-link>
-    </li>
-    <li class="nav-item">
-      <router-link to="/users/top" class="nav-link">美食達人</router-link>
+    <!-- 必須加入 :key="tab.id"，否則會報錯 -->
+    <!-- In 2.2.0+, when using v-for with a component, a key is now required. -->
+    <li v-for="tab in tabs" :key="tab.id" class="nav-item">
+      <!-- router-link 中連結屬性 ，:to 或 to 皆可以 -->
+      <!-- <router-link :to="tab.path" class="nav-link">{{ tab.title }}</router-link> -->
+      <router-link to="tab.path" class="nav-link">{{ tab.title }}</router-link>
     </li>
   </ul>
 </template>
+
+<script>
+import uuid from "uuid/v4";
+export default {
+  data() {
+    // data 屬性必須使用 function
+    return {
+      tabs: [
+        {
+          id: uuid(),
+          title: "首頁",
+          path: "/restaurants"
+        },
+        {
+          id: uuid(),
+          title: "最新動態",
+          path: "/restaurants/feeds"
+        },
+        {
+          id: uuid(),
+          title: "TOP 10 人氣餐廳",
+          path: "/restaurants/top"
+        },
+        {
+          id: uuid(),
+          title: "美食達人",
+          path: "/users/top"
+        }
+      ]
+    };
+  }
+};
+</script>
