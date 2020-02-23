43d081c1761389e60dc50da5419a0362ea63a476
Gary Wu <s19003045@gmail.com>
2020-02-23 22:43:08 +0800

    在子層建立 props 屬性以引用父層的 data

--- src/components/NewestRestaurants.vue
+++ src/components/NewestRestaurants.vue
@@ -2,30 +2,37 @@
   <div class="card">
     <div class="card-header">最新餐廳</div>
     <div class="card-body">
-      <!-- <div>
+      <div v-for="restaurant in restaurants" v-bind:key="restaurant.id">
         <h4>
-          <a href="#">Carter Howe</a>
-          <small>日本料理</small>
+          <a href="#">{{restaurant.name}}</a>
+          <small>{{restaurant.Category.name}}</small>
         </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
+        <p>{{restaurant.description}}</p>
+        {{restaurant.createdAt | fromNow}}
         <hr />
       </div>
-      <div>
-        <h4>
-          <a href="#">Dr. Carole Smith</a>
-          <small>義大利料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr />
-      </div>
-      <div>
-        <h4>
-          <a href="#">Nyah Bernier</a>
-          <small>素食料理</small>
-        </h4>
-        <p>Voluptates veritatis blanditiis ipsam. Tempora molestiae ut qui deserunt debitis sit. Impedit est rem. Blanditiis aliquid atque quo necessitatibus. Veniam sint explicabo. Est velit soluta.</p>11 days ago
-        <hr />
-      </div>-->
     </div>
   </div>
 </template>
+
+<script>
+// import moment from "moment";
+export default {
+  props: {
+    restaurants: {
+      type: Array,
+      required: true
+    }
+  }
+  // local 註冊 filter
+  // filters: {
+  //   fromNow(datetime) {
+  //     if (!datetime) {
+  //       return "-";
+  //     }
+  //     // 使用 moment 提供的 fromNow 方法
+  //     return moment(datetime).fromNow();
+  //   }
+  // }
+};
+</script>
