5e1a08140c27df1bf4e79dc2cd39d161e503c055
Gary Wu <s19003045@gmail.com>
2020-02-23 22:44:08 +0800

    引用 moment 套件，並在全域註冊 filter: fromNow 函式 ，用以轉換時間

--- src/main.js
+++ src/main.js
@@ -1,10 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import moment from "moment";
 
 Vue.config.productionTip = false
 
+// 全域註冊 filter
+Vue.filter('fromNow', function (value) {
+  if (!value) {
+    return "-";
+  }
+  // 使用 moment 提供的 fromNow 方法
+  return moment(value).fromNow();
+})
+// 建立 Vue instance
 new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
